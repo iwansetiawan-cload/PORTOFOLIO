ec7814e52af7aa283a4bdcf02dec3589e7e7f1cf
iwan setiawan <iwan.s@rds.co.id>
2024-04-23 10:54:55 +0700

    add contact us controller

---
+++ PORTOFOLIO/Areas/Admin/Controllers/ContactUsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PORTOFOLIO.Areas.Admin.Controllers
+{
+    public class ContactUsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
